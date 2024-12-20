35b975c26d7d126bec011efb66e26f1085e7edb7
nicholas harper <nicholasharper000@gmail.com>
2024-12-20 01:05:20 -0800

    Update index.php

--- hostel/admin/index.php
+++ hostel/admin/index.php
@@ -3,31 +3,26 @@ session_start();
 include('includes/config.php');
 if(isset($_POST['login']))
 {
-$username=$_POST['username'];
-$password=$_POST['password'];
-$stmt=$mysqli->prepare("SELECT username,email,password,id FROM admin WHERE (userName=?|| email=?) and password=? ");
-				$stmt->bind_param('sss',$username,$username,$password);
-				$stmt->execute();
-				$stmt -> bind_result($username,$username,$password,$id);
-				$rs=$stmt->fetch();
-				$_SESSION['id']=$id;
-				$uip=$_SERVER['REMOTE_ADDR'];
-				$ldate=date('d/m/Y h:i:s', time());
-				if($rs)
-				{
-                //  $insert="INSERT into admin(adminid,ip)VALUES(?,?)";
-   // $stmtins = $mysqli->prepare($insert);
-   // $stmtins->bind_param('sH',$id,$uip);
-    //$res=$stmtins->execute();
-					header("location:admin-profile.php");
-				}
-
-				else
-				{
-					echo "<script>alert('Invalid Username/Email or password');</script>";
-				}
-			}
-				?>
+	$username=$_POST['username'];
+	$password=$_POST['password'];
+	$stmt=$mysqli->prepare("SELECT username,email,password,id FROM admin WHERE (userName=?|| email=?) and password=? ");
+	$stmt->bind_param('sss',$username,$username,$password);
+	$stmt->execute();
+	$stmt -> bind_result($username,$username,$password,$id);
+	$rs=$stmt->fetch();
+	$_SESSION['id']=$id;
+	$uip=$_SERVER['REMOTE_ADDR'];
+	$ldate=date('d/m/Y h:i:s', time());
+	if($rs)
+	{
+		header("location:admin-profile.php");
+	}
+	else
+	{
+		echo "<script>alert('Invalid Username/Email or password');</script>";
+	}
+}
+?>
 
 <!doctype html>
 <html lang="en" class="no-js">
