1be2dba529a06fe5fc0c1d24053ff0fa8aea6aaa
nicholas harper <nicholasharper000@gmail.com>
2024-12-20 00:50:47 -0800

    Update Query v2.0.sql

--- Final Sem Clg Practical Project Task/Query v2.0.sql
+++ Final Sem Clg Practical Project Task/Query v2.0.sql
@@ -1,5 +1,3 @@
--- Query v2.0
-
 select pm.p_id, st.student_name , st.s_phone_number, st.depertment, st.room_no, pm.b_id, vs.visitor_name, w.warden_name 
 from payment AS pm inner join 
 student as st on pm.s_id = st.s_id inner join visitor as vs on vs.v_id = st.v_id  inner join 
